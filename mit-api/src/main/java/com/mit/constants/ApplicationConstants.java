fe504658b402e09e66b21702641770c0fd3f9179
Shitikantha <subham.patro89@gmail.com>
2025-02-28 14:32:19 +0530

    Delete mit-api/src/main directory

--- mit-api/src/main/java/com/mit/constants/ApplicationConstants.java
+++
@@ -1,9 +0,0 @@
-package com.mit.constants;
-
-public class ApplicationConstants {
-
-	public static final String FALSE_STR = "false";
-	public static String SUCCESS ="Success";
-	public static String FAILURE ="Failure";
-
-}
--- mit-api/src/main/java/com/mit/dao/CustomerSigninRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mit.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mit.entity.CustomerSignin;
-
-public interface CustomerSigninRepository extends JpaRepository<CustomerSignin, Long> {
-	CustomerSignin findByUsernameAndPassword(String username,String password);
-}
